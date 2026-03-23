16abbe5a26a1e3398bdc330620e479dad2858d91
Edgar Jason Husin <edgarjasonhusin@gmail.com>
2026-03-24 02:17:45 +0700

    resources edit

---
+++ resources/views/pages/login.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    {{-- <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="styleguide.css" />
+    <link rel="stylesheet" href="style.css" /> --}}
+  </head>
+  <body>
+    <div class="login-page-normal">
+      <img class="image" src="img/image.png" />
+      <div class="rectangle"></div>
+      <div class="frame">
+        <img class="img" src="img/image-3.png" />
+        <div class="div">
+          <div class="text-wrapper">Find A Teammate</div>
+          <div class="text-wrapper-2">by FILKOM UB</div>
+        </div>
+      </div>
+      <p class="p">Temukan rekan kolaborasimu di Find A Teammate!</p>
+      <p class="text-wrapper-3">Kolaborasi dalam Tim, Cari Anggota, dan Sukseskan Proyekmu!</p>
+      <div class="frame-2">
+        <div class="frame-3">
+          <div class="dikelola-wrapper"><div class="dikelola">Log In</div></div>
+          <div class="diikuti">Sign Up</div>
+        </div>
+        <div class="div-2">
+          <div class="text-wrapper-4">Log In</div>
+          <p class="text-wrapper-5">Silahkan login menggunakan akun Find A Teammate</p>
+        </div>
+        <div class="frame-wrapper">
+          <div class="frame-4">
+            <div class="div-2">
+              <div class="lorem-ipsum">Email</div>
+              <div class="input-value-wrapper"><div class="input-value">Masukkan email anda</div></div>
+            </div>
+            <div class="div-2">
+              <div class="lorem-ipsum">Password</div>
+              <div class="frame-5">
+                <div class="input-value-2">Masukkan password anda</div>
+                <img class="password-eye" src="img/password-eye.svg" />
+              </div>
+            </div>
+            <div class="buttons">
+              <button class="button"><div class="button-2">Log In</div></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
