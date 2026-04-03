770d3537773e596cdb6d6cb3a99f5fe5771b448a
wkloose <hisyamalif13@gmail.com>
2026-04-04 01:21:10 +0700

    feat : add implementation infocard

--- resources/views/pages/proyekDikelola.blade.php
+++ resources/views/pages/proyekDikelola.blade.php
@@ -7,7 +7,7 @@
 <x-back></x-back>
     <div class="w-full flex flex-col space-y-4 gap-12">
         <div class="flex flex-col gap-8">
-            <x-project.projectHeader title="Proyek 1" description="Deskripsi 1" roleTag="Role 1"></x-project.projectHeader>
+            <x-project.projectHeader name="Saya" title="Proyek 1" description="Deskripsi 1" roleTag="Role 1"></x-project.projectHeader>
             <x-button variant="primary" class="w-fit">Edit Proyek</x-button>
         </div>
 
@@ -20,6 +20,13 @@
         <div class="flex flex-col gap-4">
             <x-project.title>Role yang Diperlukan</x-project.title>
             <x-project.jobDescription role="Role 1" description="Deskripsi 1"></x-project.jobDescription>
+        </div>
+        <div class="flex flex-col gap-4">
+            <x-project.infoCard
+                title="Informasi kepada pelamar"
+                description="Dekripsi"
+                link="link"
+            />
         </div>
     </div>
 </div>
