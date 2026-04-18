fdb818ed4891f3c5a4adb5fe0da1fce918a49ec5
wkloose <hisyamalif13@gmail.com>
2026-04-18 13:54:01 +0700

    fix: fix logic for view data

--- resources/views/projects/proyekDikelola.blade.php
+++ resources/views/projects/proyekDikelola.blade.php
@@ -4,30 +4,42 @@
 
 @section('content')
 <div class="flex flex-col gap-8">
-<x-back></x-back>
+    <x-back></x-back>
     <div class="w-full flex flex-col space-y-4 gap-12">
         <div class="flex flex-col gap-8">
-            <x-project.projectHeader name="Saya" title="Proyek 1" description="Deskripsi 1" roleTag="Role 1"></x-project.projectHeader>
+            <x-project.projectHeader 
+                name="Dikelola oleh {{ $project['owner'] ?? 'Unknown' }}" 
+                title="{{ $project['project_name'] }}" 
+                description="{{ $project['project_plan'] }}" 
+                :project_field="array_map('trim', explode(',', $project['project_field'] ?? ''))"
+            />
             <x-button variant="primary" class="w-fit">Edit Proyek</x-button>
         </div>
 
         <div>
             <h1 class="text-xl font-bold text-primary-8">Pelamar yang Masuk</h1>
+            <!-- nanti bisa looping pelamar dari JSON juga -->
         </div>
 
-        <x-project.titleCaption title="Rancangan Proyek" caption="Deskripsi Proyek"></x-project.titleCaption>
+        <x-project.titleCaption 
+            title="Rancangan Proyek" 
+            caption="{{ $project['project_plan'] }}"
+        />
 
         <div class="flex flex-col gap-4">
             <x-project.title>Role yang Diperlukan</x-project.title>
-            <x-project.jobDescription role="Role 1" description="Deskripsi 1"></x-project.jobDescription>
+            @foreach($project['roles'] ?? [] as $role)
+                <x-project.jobDescription role="{{ $role }}" description="Deskripsi untuk {{ $role }}"></x-project.jobDescription>
+            @endforeach
         </div>
+
         <div class="flex flex-col gap-4">
             <x-project.infoCard
                 title="Informasi kepada pelamar"
-                description="Dekripsi"
-                link="link"
+                description="{{ $project['accepted_info'] }}"
+                link="{{ $project['link'] ?? '#' }}"
             />
         </div>
     </div>
 </div>
-@endsection 
+@endsection
