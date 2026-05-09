f45154017ea5501c6c7a1354564c42eed4f0d95d
wkloose <hisyamalif13@gmail.com>
2026-05-09 21:05:10 +0700

    fix(db): resolve foreign key constraint mismatch in project migrations

--- database/migrations/500000_create_project_applications_table.php
+++ database/migrations/500000_create_project_applications_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->unique(['user_id', 'project_id', 'roles_id']);
 
             $table->foreign('user_id')
-                  ->references('user_id')
+                  ->references('id')
                   ->on('users')
                   ->onDelete('cascade');
 
